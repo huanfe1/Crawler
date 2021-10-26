98a60dfb33c74cfe279befe05af5fe27b8d561b2
幻非 <41602338+huanfeiiiii@users.noreply.github.com>
2021-10-26 13:04:09 +0800

    优化结构

--- information.py
+++ information.py
@@ -9,21 +9,18 @@ import requests
 import re
 
 
-def information(url):
+def information(self):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/92.0.4515.159 Safari/537.36 '
     }
 
-    response = requests.get(url=url, headers=headers)
-    html_data = re.findall('<span><span><span><span>(.*?)</span></span></span>', response.text) + \
-                re.findall('抖音号： <!-- -->(.*?)</p><p class=', response.text) + \
-                re.findall("已有(.*?)个粉丝", response.text) + \
-                re.findall("收获了(.*?)个喜欢，欢迎观看", response.text) + \
-                re.findall('作品<span class="_03811320ee25b81d1c705fae532572ec-scss">(.*?)</span>', response.text)
-    get_information = {"name": html_data[0], "ID": html_data[1], "fans": html_data[2], "like": html_data[3],
-                       "work": html_data[4], }
-    return get_information
+    response = requests.post(url=self, headers=headers).text
+
+    html_data = re.findall('content="(.*?)"/><meta data-react-helmet=', response)
+    get_inf = re.findall("(.*?)：(.*?)TA的抖音号是(.*?)，已有(.*?)个粉丝，收获了(.*?)个喜欢", html_data[1])[0]
+    text = f"TA的抖音ID为：{get_inf[0]}，抖音号是42984577{get_inf[2]}，已有{get_inf[3]}个粉丝，收获了{get_inf[4]}个喜欢"
+    print(text)
 
 
 if __name__ == '__main__':
