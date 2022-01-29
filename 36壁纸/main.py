f9d5f9b842e0dd97c9c984a188959a49d7095fad
幻非 <eg1981400335@gmail.com>
2022-01-29 20:13:58 +0800

    Update main.py

--- 36壁纸/main.py
+++ 36壁纸/main.py
@@ -6,57 +6,32 @@
 """
 
 import re
-import time
+import threading
 import requests
 import os
 
-if not os.path.exists("resources"):
-    os.mkdir("resources")
-history_path = 'resources' + '\\' + 'history.txt'
-
-if not os.path.exists(history_path):
-    file = open(history_path, 'w')
-    file.close()
-
-with open(history_path, 'r', encoding="utf-8") as file:
-    history = file.read()
-
-save_pic_list = [i.replace('.jpg', '') for i in os.listdir('resources')]
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
 }
 
-
-def grab(self):
-    """抓取网页壁纸"""
-    k = 0
-    for j in range(5, 23):
-        url = f'{self}index_{j}.html'
-        if not history.count(url):
-            print(url)
-            url_source = requests.get(url=url, headers=headers).text
-            url_state = requests.get(url=url, headers=headers).status_code
-            if url_state == 200:
-                get_tittle_list = re.findall(r'" alt="(.*?)" title="', url_source)
-                for tittle in get_tittle_list:
-                    if not save_pic_list.count(tittle):
-                        include_pic_url = url_source[url_source.find(tittle) - 270:url_source.find(tittle) - 210]
-                        pic_url = re.findall('href="(.*?)"', include_pic_url)[0]
-                        pic_source = requests.get(url=pic_url, headers=headers).text
-                        pic_path = re.findall('<a href="(.*?)" target="_blank" title=', pic_source)[0]
-                        pic_content = requests.get(url=pic_path, headers=headers).content
-                        with open('resources' + '\\' + tittle + '.jpg', 'wb')as f:
-                            f.write(pic_content)
-                        print(k, tittle, pic_url)
-                        time.sleep(2)
-                        k += 1
-                with open(history_path, 'a', encoding="utf-8") as f:
-                    f.write(url+'\n')
-                time.sleep(2)
-            elif url_state == 404:
-                break
+if not os.path.exists('resources'):
+    os.mkdir('resources')
 
 
-if __name__ == '__main__':
-    grab("https://desk.3gbizhi.com/deskMX/")
+def save_pic(url, tittle):
+    response = requests.get(url=url, headers=headers).content
+    with open(f'resources\\{tittle}.jpg', 'wb') as f:
+        f.write(response)
+    print(url)
+
+
+def craw(url):
+    response = requests.get(url=url, headers=headers).text.encode('GBK', 'ignore').decode('GBK')
+    get_data = re.findall('lazysrc2x="(.*?).556.308.jpg 2x" alt="(.*?)" title="', response)
+    for i in get_data:
+        threading.Thread(target=save_pic, args=(i[0], i[1])).start()
+
+
+for i in range(1, 24):
+    craw(f'https://desk.3gbizhi.com/deskMV/index_{i}.html')
