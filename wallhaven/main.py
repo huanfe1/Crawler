3389450617d9f1ae1e709512261271e7e5dd3ea9
幻非 <eg1981400335@gmail.com>
2022-03-11 21:05:28 +0800

    update wallhaven

---
+++ wallhaven/main.py
@@ -0,0 +1,37 @@
+# _*_ coding:utf-8 _*_
+import os
+import random
+import re
+import time
+import requests
+import bs4
+from threading import Thread
+
+try:
+    os.mkdir('resources')
+except FileExistsError:
+    pass
+
+
+def craw(li):
+    img_id = re.findall('data-wallpaper-id="(.*?)"', str(li))[0]
+    if '<span class="png">' in str(li):
+        img_type = 'png'
+    else:
+        img_type = 'jpg'
+    img_url = f'https://w.wallhaven.cc/full/{img_id[:2]}/wallhaven-{img_id}.{img_type}'
+    print(img_url)
+    with open(f'resources\\{img_id}.{img_type}', 'wb') as file:
+        file.write(requests.get(url=img_url).content)
+    time.sleep(random.randint(1, 3))
+
+
+def main(url):
+    response = requests.get(url).text
+    soup = bs4.BeautifulSoup(response, features="html.parser")
+    lis = soup.find('section', attrs={'class': 'thumb-listing-page'}).find_all('li')
+    for li in lis:
+        Thread(target=craw, args=(li,)).start()
+
+
+main('https://wallhaven.cc/toplist?page=2')
